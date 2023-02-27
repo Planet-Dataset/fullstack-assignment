5d502e3c0e236e4a40cf797404870a412049bd67
jancho <jancho@usal.es>
2023-02-27 14:08:04 +0100

    feat: Separate db and back services in makefiles.

--- backend.mk
+++ backend.mk
@@ -1,32 +1,10 @@
-DB_USER=root
-DB_PASS=pass
-
 backend-build:
-	${DOCKER_COMPOSE} build
-
-backend-build-slim:
-	${DOCKER_COMPOSE} -f docker-compose.slim.yml build
+	${DOCKER_COMPOSE} build api
 
 backend-up:
-	${DOCKER_COMPOSE} up
-
-backend-up-slim:
-	${DOCKER_COMPOSE} -f docker-compose.slim.yml up
+	${DOCKER_COMPOSE} up api
 
 backend-down:
-	${DOCKER_COMPOSE} down
+	${DOCKER_COMPOSE} down api
 
-backend-dump:
-	${DOCKER_COMPOSE} exec -T mongodb sh -c 'mongodump --authenticationDatabase admin -u ${DB_USER} -p ${DB_PASS} -o /dbdump'
-
-backend-load:
-	${DOCKER_COMPOSE} exec -T mongodb sh -c 'mongorestore --authenticationDatabase admin -u ${DB_USER} -p ${DB_PASS} --dir=/dbdump'
-
-backend-dbshell:
-	${DOCKER_COMPOSE} exec mongodb mongo --authenticationDatabase admin -u ${DB_USER} -p ${DB_PASS}
-
-backend-test: backend-dump
-	echo ""
-	make backend-load
-
-$PHONY: backend-build backend-build-slim backend-up backend-up-slim backend-down backend-dump backend-load backend-dbshell backend-test
+$PHONY: backend-build backend-up backend-down
